1195406bd7a8ea8ebe233448ee6b0d8c3727f289
Nikita <nikitadedov66@gmail.com>
2020-12-10 02:02:21 +0200

    camera controller script created

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public static CameraController Instance;
+
+    [SerializeField]
+    public Camera mainCamera;
+
+    [SerializeField]
+    private float _speedOfFollowing;
+
+    [SerializeField]
+    private float _zPosAddition;
+
+    [SerializeField]
+    private float _heightOfCamera;
+
+    private void Start() {      
+        if(Instance != null) {
+            Destroy(this);
+        }
+        Instance = this;
+    }
+
+    void Update() {
+        mainCamera.transform.position = Vector3.Lerp(
+            mainCamera.transform.position, 
+            new Vector3(Player.playerTransform.transform.position.x, Player.playerTransform.transform.position.y + _heightOfCamera, Player.playerTransform.transform.position.z + -_zPosAddition),
+            _speedOfFollowing);
+    }
+}
