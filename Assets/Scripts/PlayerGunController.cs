619ca460851d56b0f2ad0fff52824933626849fc
Nikita <nikitadedov66@gmail.com>
2020-12-10 02:03:15 +0200

    playerguncontroller created

---
+++ Assets/Scripts/PlayerGunController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunController : MonoBehaviour
+{
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            PlayerGun.Shoot();
+        }
+    }
+
+
+}
