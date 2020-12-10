7f8a018da98a6a7fb5a4bd36bb56c4a0f9cba59e
Nikita <nikitadedov66@gmail.com>
2020-12-10 02:03:53 +0200

    additional utils created

---
+++ Assets/Scripts/GroundPointFromMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GroundPointFromMouse {
+
+    public static Vector3 GetPos() {
+        Ray ray = CameraController.Instance.mainCamera.ScreenPointToRay(Input.mousePosition);
+       
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f)) {
+            var targetPos = hitInfo.point;
+            return targetPos;
+        }
+
+        return Vector3.zero;
+    }
+
+
+
+}
