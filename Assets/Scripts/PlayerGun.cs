77667df7ba66a64d73f175ddf83e64e231305be8
Nikita <nikitadedov66@gmail.com>
2020-12-10 02:02:52 +0200

    playergun script created

---
+++ Assets/Scripts/PlayerGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+    [SerializeField]
+    protected float damage;
+
+    [SerializeField]
+    protected string name;
+
+    public static PlayerGun currentGun;
+
+    [SerializeField]
+    protected AudioClip shootSound;
+
+    public static void Shoot() {
+        Debug.Log("Shoot");
+    }
+
+
+}
